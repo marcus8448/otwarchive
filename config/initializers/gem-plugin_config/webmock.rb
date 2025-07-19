1cef7908527ea3f35cb854bfc18daf41cc7e8d23
Marcus Low <mlow16@outlook.com>
2025-07-18 23:57:05 -0700

    AO3-? Only allow net connect on start for capybara

--- config/initializers/gem-plugin_config/webmock.rb
+++ config/initializers/gem-plugin_config/webmock.rb
@@ -1,5 +1,5 @@
 if Rails.env.test?
   # https://stackoverflow.com/questions/59632283/chromedriver-capybara-too-many-open-files-socket2-for-127-0-0-1-port-951
-  WebMock.allow_net_connect!(net_http_connect_on_start: true)
+  WebMock.allow_net_connect!(net_http_connect_on_start: Capybara.server_host)
   WebMock.enable!
 end
